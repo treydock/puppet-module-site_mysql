1bfb4b8cdd335e77e6c3b5cddf1898dc0cc609f8
Trey Dockendorf <treydock@gmail.com>
2015-02-27 18:01:43 -0600

    * Add site_mysql::backup class to handle formatting LVM volume for backups and mounting the device

---
+++ manifests/backup.pp
@@ -0,0 +1,51 @@
+# == Class: site_mysql::mounts
+#
+# Manage mount points for MySQL instance
+#
+class site_mysql::backup (
+  $backup_pv,
+  $manage_mounts        = true,
+  $manage_lvm           = true,
+  $backup_device        = undef,
+  $backup_path          = undef,
+  $backup_fstype        = 'xfs',
+  $backup_mount_options = 'noatime,nodiratime,nobarrier,defaults',
+  $backup_lv_name       = 'lv_mysql_backup',
+  $backup_vg_name       = 'vg_mysql_backup',
+  $backup_size          = undef,
+  $backup_extents       = undef,
+) {
+
+  validate_bool($manage_mounts, $manage_lvm)
+
+  include mysql::server
+  include mysql::server::backup
+
+  $backup_path_real   = pick($backup_path, $mysql::server::backup::backupdir)
+  $backup_device_real = pick($backup_device, "/dev/${backup_vg_name}/${backup_lv_name}")
+
+  if $manage_lvm {
+    lvm::volume { $backup_lv_name:
+      ensure  => 'present',
+      vg      => $backup_vg_name,
+      pv      => $backup_pv,
+      fstype  => $backup_fstype,
+      size    => $backup_size,
+      extents => $backup_extents,
+      before  => Mount['mysql-backupdir'],
+    }
+  }
+
+  if $manage_mounts {
+    mount { 'mysql-backupdir':
+      ensure  => 'mounted',
+      name    => $backup_path_real,
+      atboot  => true,
+      device  => $backup_device_real,
+      fstype  => $backup_fstype,
+      options => $backup_mount_options,
+      before  => File['mysqlbackupdir'],
+    }
+  }
+
+}
---
+++ spec/classes/site_mysql_backup_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe 'site_mysql::backup' do
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      it { should compile.with_all_deps }
+    end
+  end
+
+  let(:facts) do
+    on_supported_os['centos-7-x86_64']
+  end
+  let(:params) {{ :backup_pv => '/dev/vdc' }}
+  let(:pre_condition) do
+    "class { 'mysql::server::backup':
+      backupuser      => 'backup',
+      backuppassword  => 'backup',
+      backupdir       => '/opt/mysql_backups',
+    }"
+  end
+
+  it { should create_class('site_mysql::backup') }
+
+  it do
+    should contain_lvm__volume('lv_mysql_backup').with({
+      :ensure => 'present',
+      :vg     => 'vg_mysql_backup',
+      :pv     => '/dev/vdc',
+      :fstype => 'xfs',
+      :size   => nil,
+      :before => 'Mount[mysql-backupdir]',
+    })
+  end
+
+  it do
+    should contain_mount('mysql-backupdir').with({
+      :ensure  => 'mounted',
+      :name    => '/opt/mysql_backups',
+      :atboot  => 'true',
+      :device  => '/dev/vg_mysql_backup/lv_mysql_backup',
+      :fstype  => 'xfs',
+      :options => 'noatime,nodiratime,nobarrier,defaults',
+      :before  => 'File[mysqlbackupdir]',
+    })
+  end
+
+end
